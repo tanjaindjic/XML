d2a8145831b12a3a00d13d735f42ab7b3870c01d
ilijagrbic <ilijagrb.ns@gmail.com>
2018-06-18 23:54:09 +0200

    Evo :D

--- projekat/src/main/java/XmlWeb/service/SmestajService.java
+++ projekat/src/main/java/XmlWeb/service/SmestajService.java
@@ -21,7 +21,7 @@ public class SmestajService {
 	
 	public Collection<Smestaj> getAllSmestajSimple(SearchDTO ser){
 		System.out.println(ser);
-		return smestajRepository.findSimpleSearch(ser.getFrom(), ser.getTo(), ser.getDestination(), ser.getHowManyPeople());
+		return smestajRepository.findAll();//findSimpleSearch(ser.getFrom(), ser.getTo(), ser.getDestination(), ser.getHowManyPeople());
 	}
 	
 	public Collection<Smestaj> getAllSmestajAdv(SearchDTO ser){
